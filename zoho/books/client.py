da24e7aad2d0ec346c256e72e70af51be58abaa9
Valentin Lehuger <valentin@lehuger.fr>
2019-01-17 10:31:11 +0100

    add organization in zoho books client

--- zoho/books/client.py
+++ zoho/books/client.py
@@ -14,6 +14,10 @@ class Client(CommonClient):
         ('organizations', {'list_name': 'organizations'})
     ])
 
+    def __init__(self, client_id, client_secret, redirect_uri, scope, access_type, organization_id=None, refresh_token=None):
+        super(Client, self).__init__(client_id, client_secret, redirect_uri, scope, access_type, refresh_token)
+        self._organization_id = organization_id
+
     @property
     def available_read_modules(self):
         return list(self.READ_MODULE_LIST)
@@ -30,10 +34,13 @@ class Client(CommonClient):
         module_conf = self.READ_MODULE_LIST[module_name]
 
         url = self.BASE_URL + str(module_name)
-        response = self._get(url)
+
+        base_params = {'organization_id': self._organization_id} if self._organization_id is not None else {}
+
+        response = self._get(url, params=base_params)
         all_data = response[module_conf['list_name']]
-        while response['page_context']['has_more_page']:
+        while response.get('page_context', {}).get('has_more_page'):
             page = response['page_context']['page']
-            response = self._get(url, params={'page': int(page) + 1})
+            response = self._get(url, params=dict({'page': int(page) + 1}, **base_params))
             all_data.extend(response[module_conf['list_name']])
         return all_data
